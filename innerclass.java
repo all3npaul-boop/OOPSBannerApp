ffff623bc5ead1d7cdcbd580df875d2ee01e7285
all3npaul-boop <all3n.paul@gmail.com>
2026-03-03 08:41:21 +0530

    Create innerclass.java

---
+++ innerclass.java
@@ -0,0 +1,67 @@
+import java.util.HashMap;
+import java.util.Map;
+public class OOPSBanner {
+    public static void main(String[] args) {
+        String word = "OOPS";
+        CharacterPatternMap patternMap = new CharacterPatternMap();
+        displayBanner(word, patternMap);
+    }
+    public static void displayBanner(String text, CharacterPatternMap map) {
+        int height = 9;
+        for (int row = 0; row < height; row++) {
+            StringBuilder lineBuilder = new StringBuilder();
+            for (char ch : text.toCharArray()) {
+                String[] pattern = map.getPattern(ch);
+                if (pattern != null) {
+                    lineBuilder.append(pattern[row]).append("  ");
+                }
+            }
+            System.out.println(lineBuilder);
+        }
+    }
+    static class CharacterPatternMap {
+        private final Map<Character, String[]> patternMap;
+        public CharacterPatternMap() {
+            patternMap = new HashMap<>();
+            initializePatterns();
+        }
+        private void initializePatterns() {
+            patternMap.put('O', new String[]{
+                    "   *****  ",
+                    " **     ** ",
+                    "**       **",
+                    "**       **",
+                    "**       **",
+                    "**       **",
+                    "**       **",
+                    " **     ** ",
+                    "   *****  "
+            });
+            patternMap.put('P', new String[]{
+                    "******   ",
+                    "**    ** ",
+                    "**     **",
+                    "**    ** ",
+                    "******   ",
+                    "**       ",
+                    "**       ",
+                    "**       ",
+                    "**       "
+            });
+            patternMap.put('S', new String[]{
+                    "   *****   ",
+                    "**    **",
+                    "**     ",
+                    "**       ",
+                    "  *****   ",
+                    "      ** ",
+                    "       **",
+                    "**     **",
+                    "   *****   "
+            });
+        }
+        public String[] getPattern(char character) {
+            return patternMap.get(Character.toUpperCase(character));
+        }
+    }
+}
